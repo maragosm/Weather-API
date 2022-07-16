ba18fabdb8697320c38423c769b186037b955e58
Michael Maragos <maragosm@aol.com>
2022-07-16 13:18:44 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Weather_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
